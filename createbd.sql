a91bb25f8ed3d3f2f8d3a7eb5a6c0c2cdf58e1cf
barabaratalita <84820396+barabaratalita@users.noreply.github.com>
2023-10-18 20:21:55 -0300

    Update createbd.sql

--- createbd.sql
+++ createbd.sql
@@ -72,3 +72,22 @@ create table limpeza_manutencao(
 
 /* criando as consultas*/
 
+/* Cria uma tabela com as salas que estao reservadas para agora */
+salas_reservadas as (
+	SELECT s.id_sala,
+		FROM sala s, reserva r
+			WHERE
+				 r.id_sala = s.id_sala
+				 and (date(r.horario_inicio) <=  date(NOW())
+				 and date(r.horario_fim) >= date(NOW())
+)
+
+/* Pega todas as salas que nao pertencem ao conjunto das reservadas.: salas - salas_reservadas */ 
+SELECT s.id_sala, s.numero, s.lotacao, s.tipo_sala, s.data_show, s.climatizacao, s.obs, s.sala_especial
+	FROM sala s, salas_reservadas sr
+		WHERE
+			s.id_sala != sr.id_sala
+
+
+            
+
