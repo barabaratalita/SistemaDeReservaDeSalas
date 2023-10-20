8dd7674762ddc77e314f7907bdf133e8e874e4cb
barabaratalita <84820396+barabaratalita@users.noreply.github.com>
2023-10-20 19:20:53 -0300

    Update createbd.sql

--- createbd.sql
+++ createbd.sql
@@ -1,20 +1,21 @@
 
 /*  criar a base de dos caso nao exista*/
 CREATE DATABASE IF NOT EXISTS sistema_reserva_salas_db;
+use sistema_reserva_salas_db;
 
 /* criando as tabelas*/
 
 create table usuario(
     email varchar(30) primary key,
     nome varchar(30) not null,
-    senha varchar() not null,
+    senha varchar(250) not null,
     contato_telefone varchar(14) ,
     token_recuperacao_senha varchar(250)
 );
 
-ALTER TABLE usuario ADD CONSTRAINT MIN_LENGTH_EMAIL CHECK (CHAR_LENGTH(email) >= 1);
-ALTER TABLE usuario ADD CONSTRAINT MIN_LENGTH_NOME CHECK (CHAR_LENGTH(nome) >= 1);
-ALTER TABLE usuario ADD CONSTRAINT MIN_LENGTH_SENHA CHECK (CHAR_LENGTH(senha) >= 12);
+ALTER TABLE usuario ADD CONSTRAINT tamanho_min_email CHECK (CHAR_LENGTH(email) >= 5);
+ALTER TABLE usuario ADD CONSTRAINT tamanho_min_nome CHECK (CHAR_LENGTH(nome) >= 3);
+ALTER TABLE usuario ADD CONSTRAINT tamanho_min_senha CHECK (CHAR_LENGTH(senha) >= 12);
 
 create table discente(
     matricula integer primary key auto_increment,
@@ -38,47 +39,50 @@ create table fucionario_tercerizado(
 create table tecnicos_adm(
     cpf char(11) not null primary key,
     tec_email varchar(30),
-    foreign key(tec_email) references usuario(email),
+    foreign key(tec_email) references usuario(email)
     
 );
 
 create table sala(
-
-    id_sala integer not null primary key,
+    id_sala integer not null primary key auto_increment,
     numero integer,
     lotacao integer,
-    tipo_sala varchar(),
+    tipo_sala varchar(250),
     data_show boolean,
     crimatizacao boolean,
-    obs varchar(),
+    obs varchar(250),
     sala_especial boolean
 );
 
 create table reserva(
-
-    id_reserva integer not null primary key,
-    id_sala integer  references sala(id_sala),
-    motivo_reserva varchar(),
-    recursos_adicionais varchar(),
+    id_reserva integer not null primary key auto_increment,
+    id_sala integer not null,
+    motivo_reserva varchar(250),
+    recursos_adicionais varchar(250),
     horario_inicio timestamp,
     horario_fim timestamp,
-    token_reserva varchar(),
-    confirmado boolean
+    token_reserva varchar(250),
+    confirmado boolean,
+    foreign key(id_sala) references sala(id_sala)
 );
 
 
 create table reserva_responsavel(
-    id_reserva integer references reserva(id_reserva), 
-    responsavel varchar() references usuario(email)
-    );
+    id_reserva integer, 
+    responsavel varchar(250), 
+    foreign key(id_reserva) references reserva(id_reserva),
+    foreign key(responsavel) references usuario(email)
+);
 
 
 create table limpeza_manutencao(
-
-    cpf_funcionario char(11) references fucionario_tercerizado(cpf),
-    id_sala integer references sala(id_sala),
-    data_hora timestamp
+    cpf_funcionario char(11), 
+    id_sala integer,
+    data_hora timestamp, 
+    foreign key(cpf_funcionario) references fucionario_tercerizado(cpf),
+    foreign key(id_sala) references sala(id_sala)
 );
+
 
 
 
