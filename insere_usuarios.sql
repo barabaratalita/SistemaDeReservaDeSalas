2c96493b1ee960913bea2e1deb02cf53844b2d44
barabaratalita <84820396+barabaratalita@users.noreply.github.com>
2023-10-20 20:49:35 -0300

    Update

---
+++ insere_usuarios.sql
@@ -0,0 +1,121 @@
+
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email0@email.com","nome 0", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email1@email.com","nome 1", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email2@email.com","nome 2", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email3@email.com","nome 3", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email4@email.com","nome 4", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email5@email.com","nome 5", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email6@email.com","nome 6", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email7@email.com","nome 7", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email8@email.com","nome 8", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email9@email.com","nome 9", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email10@email.com","nome 10", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email11@email.com","nome 11", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email12@email.com","nome 12", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email13@email.com","nome 13", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email14@email.com","nome 14", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email15@email.com","nome 15", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email16@email.com","nome 16", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email17@email.com","nome 17", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email18@email.com","nome 18", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email19@email.com","nome 19", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email20@email.com","nome 20", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email21@email.com","nome 21", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email22@email.com","nome 22", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email23@email.com","nome 23", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email24@email.com","nome 24", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email25@email.com","nome 25", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email26@email.com","nome 26", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email27@email.com","nome 27", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email28@email.com","nome 28", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email29@email.com","nome 29", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email30@email.com","nome 30", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email31@email.com","nome 31", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email32@email.com","nome 32", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email33@email.com","nome 33", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email34@email.com","nome 34", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email35@email.com","nome 35", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email36@email.com","nome 36", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email37@email.com","nome 37", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email38@email.com","nome 38", "jsk232d3333dddsdl", "009909093", null);
+insert into usuario(email, nome, senha, contato_telefone, token_recuperacao_senha)
+                                        values("email39@email.com","nome 39", "jsk232d3333dddsdl", "009909093", null);
+insert into discente(di_email, matricula) values ("email0@email.com", 0);
+insert into discente(di_email, matricula) values ("email1@email.com", 1);
+insert into discente(di_email, matricula) values ("email2@email.com", 2);
+insert into discente(di_email, matricula) values ("email3@email.com", 3);
+insert into discente(di_email, matricula) values ("email4@email.com", 4);
+insert into discente(di_email, matricula) values ("email5@email.com", 5);
+insert into discente(di_email, matricula) values ("email6@email.com", 6);
+insert into discente(di_email, matricula) values ("email7@email.com", 7);
+insert into discente(di_email, matricula) values ("email8@email.com", 8);
+insert into discente(di_email, matricula) values ("email9@email.com", 9);
+insert into doscente(do_email, siape) values ("email10@email.com", 10);
+insert into doscente(do_email, siape) values ("email11@email.com", 11);
+insert into doscente(do_email, siape) values ("email12@email.com", 12);
+insert into doscente(do_email, siape) values ("email13@email.com", 13);
+insert into doscente(do_email, siape) values ("email14@email.com", 14);
+insert into doscente(do_email, siape) values ("email15@email.com", 15);
+insert into doscente(do_email, siape) values ("email16@email.com", 16);
+insert into doscente(do_email, siape) values ("email17@email.com", 17);
+insert into doscente(do_email, siape) values ("email18@email.com", 18);
+insert into doscente(do_email, siape) values ("email19@email.com", 19);
+insert into fucionario_tercerizado(fu_email, cpf) values ("email20@email.com", 20);
+insert into fucionario_tercerizado(fu_email, cpf) values ("email21@email.com", 21);
+insert into fucionario_tercerizado(fu_email, cpf) values ("email22@email.com", 22);
+insert into fucionario_tercerizado(fu_email, cpf) values ("email23@email.com", 23);
+insert into fucionario_tercerizado(fu_email, cpf) values ("email24@email.com", 24);
+insert into fucionario_tercerizado(fu_email, cpf) values ("email25@email.com", 25);
+insert into fucionario_tercerizado(fu_email, cpf) values ("email26@email.com", 26);
+insert into fucionario_tercerizado(fu_email, cpf) values ("email27@email.com", 27);
+insert into fucionario_tercerizado(fu_email, cpf) values ("email28@email.com", 28);
+insert into fucionario_tercerizado(fu_email, cpf) values ("email29@email.com", 29);
+insert into tecnicos_adm(tec_email, cpf) values ("email30@email.com", "00000000000");
+insert into tecnicos_adm(tec_email, cpf) values ("email31@email.com", "00000000000");
+insert into tecnicos_adm(tec_email, cpf) values ("email32@email.com", "00000000000");
+insert into tecnicos_adm(tec_email, cpf) values ("email33@email.com", "00000000000");
+insert into tecnicos_adm(tec_email, cpf) values ("email34@email.com", "00000000000");
+insert into tecnicos_adm(tec_email, cpf) values ("email35@email.com", "00000000000");
+insert into tecnicos_adm(tec_email, cpf) values ("email36@email.com", "00000000000");
+insert into tecnicos_adm(tec_email, cpf) values ("email37@email.com", "00000000000");
+insert into tecnicos_adm(tec_email, cpf) values ("email38@email.com", "00000000000");
+insert into tecnicos_adm(tec_email, cpf) values ("email39@email.com", "00000000000");
