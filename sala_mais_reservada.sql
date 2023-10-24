f7598441e821c3def1e3a88acedc8c9b06409be1
barabaratalita <84820396+barabaratalita@users.noreply.github.com>
2023-10-24 09:29:51 -0300

    Update sala_mais_reservada.sql

--- sala_mais_reservada.sql
+++ sala_mais_reservada.sql
@@ -1,12 +1,19 @@
-/*Qual a sala mais utilizada*/
-/* criando uma tabela  com a quantidade de reservas por sala*/
 use sistema_reserva_salas_db;
-reservas_por_sala as (
-    SELECT id_sala, numero, COUNT(id_reserva) AS contagem_reservas 
-        FROM reserva
-        GROUP BY id_sala, numero)
-/* selecionando a sala o a maior quantidade de reserva*/
 
-SELECT *
-    FROM reservas_por_sala 
-        WHERE contagem_reservas = MAX(contagem_reservas);
+/* Criando uma tabela com a quantidade de reservas por sala */
+WITH reservas_por_sala AS (
+    SELECT id_sala, COUNT(id_reserva) AS contagem_reservas 
+    FROM reserva
+    GROUP BY id_sala
+)
+
+/* Encontrando o valor máximo de contagem de reservas */
+, max_contagem AS (
+    SELECT MAX(contagem_reservas) AS max_reservas
+    FROM reservas_por_sala
+)
+
+/* Selecionando a(s) sala(s) com a maior quantidade de reservas */
+SELECT id_sala, contagem_reservas
+FROM reservas_por_sala
+WHERE contagem_reservas = (SELECT max_reservas FROM max_contagem);
