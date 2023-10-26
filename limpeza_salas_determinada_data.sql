f6c848ab0fff5d381e964b4bb5562af212a1111d
lucasraphael02 <lucasraphael@aluno.ufrb.edu.br>
2023-10-26 13:07:09 -0300

    update limpeza_salas_determinada_data

--- limpeza_salas_determinada_data.sql
+++ limpeza_salas_determinada_data.sql
@@ -1,24 +1,12 @@
 /* Quais salas foram limpas em determinado dia? */
 use sistema_reserva_salas_db;
 
-WITH sala_para_limpeza AS (
-    SELECT DISTINCT id_sala
-    FROM limpeza_manutencao
-    WHERE tipo = "limpeza"
-); 
-
-WITH sala_limpa_data AS ( 
-    SELECT sala_para_limpeza.*,reserva.*
-    FROM sala_para_limpeza
-    INNER JOIN reserva ON sala_para_limpeza.id_sala = reserva.id_sala
-    WHERE reserva.data_horario_fim = 'YYYY-MM-DD'
-);
-
-WITH id_sala_limpa_data AS (
-    SELECT sala_limpa_data.*
-    FROM sala_limpa_data
-    INNER JOIN sala ON sala_limpa_data.id_sala = sala.id_sala
-);
-
 SELECT DISTINCT numero
-    FROM id_sala_limpa_data
+FROM (
+	SELECT id_sala
+    FROM limpeza_manutencao
+    WHERE tipo = "limpeza" and data_hora_fim > "2023/10/26" and data_hora_fim < "2023/10/27"
+) AS A
+INNER JOIN sala ON sala.id_sala = A.id_sala
+
+
